fdd6f385460e32c0bf9b9f08133d7c905c1c39ef
Oswaldo J Plazola <plazolas@gmail.com>
2017-01-14 14:04:44 -0500

    moved debug statements to footer

--- _includes/_footer.php
+++ _includes/_footer.php
@@ -1,3 +1,8 @@
 <footer>
-    <p class="text-center"> &copy; <?php echo date('Y') ?> All Inc powered by Oz Enterprises - All Rights Reserved</p>
+    <p class="text-center"> &copy; <?php echo date('Y') ?> Secured Dashboard powered by Free Software Enterprises - No Rights Reserved</p>
 </footer>
+<?php
+if($_SESSION['debug'] == '1') {
+    include '_includes/_debug.php';
+}
+?>
