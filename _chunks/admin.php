1fb735fb4883cd4c6ea88a15d560c54904908962
Oswaldo J Plazola <plazolas@gmail.com>
2017-01-14 14:33:20 -0500

    resized images

--- _chunks/admin.php
+++ _chunks/admin.php
@@ -2,13 +2,12 @@
 <table width="98%" border="0" style="margin-left:5px;">
 <tr><td colspan="4">&nbsp;</td></tr>	
   <tr>   
-    <td><a href="practices.php"><img src="images/tr.png" width="100%"></a></td>
-    <td><a href="_qualquiz.php"><img src="images/salesreg.jpg" style="width: 100%;height: 291px"></a></td>
+    <td><a href="practices.php"><img src="images/tr.png" style="width: 500px;height: 291px"></a></td>
+    <td><a href="_qualquiz.php"><img src="images/salesreg.jpg" style="width: 500px;height: 291px"></a></td>
   </tr>
 	<tr><td colspan="4">&nbsp;</td></tr>
   <tr>
-    <td><a href="quizes.php"><img src="images/atc.png" width="100%"></a></td>
-    <td><a href="practice_reg.php"><img src="images/reg.png" style="width: 100%;height: 310px"></a></td>
+    <td><a href="quizes.php"><img src="images/atc.png" style="width: 500px;height: 310px"></a></td>
+    <td><a href="practice_reg.php"><img src="images/reg.png" style="width: 500px;height: 310px"></a></td>
   </tr> 
 </table>
-            
